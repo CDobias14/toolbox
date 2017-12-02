e5a02c539c38ad006a72d692b457809571e0a09d
Christian Dobias <32696880+CDobias14@users.noreply.github.com>
2017-12-01 16:00:38 -0800

    Update nodalParentConstraint.py

--- nodalParentConstraint.py
+++ nodalParentConstraint.py
@@ -3,7 +3,6 @@
 # As with a standard parent constraint, select the driver first and the driven second.
 # Unlike a standard parent constraint, you cannot constrain an object to multiple drivers. The driven object can only accept a single driver.
 # Function automatically calculates and includes offset.
-# WARNING: Function does not behave well when driving joints! Joints may serve as drivers, but may break if driven.
 ###
 
 def nodalParentConstraint(*args):
@@ -18,7 +17,7 @@ def nodalParentConstraint(*args):
     # Create multMatrix and decomposeMatrix nodes
     cmds.shadingNode('decomposeMatrix', asUtility = True, n = '{}_mtx2srt'.format(sel[1]))
     cmds.shadingNode('multMatrix', asUtility = True, n = '{}_mtxOffset'.format(sel[1]))
-    
+
     # Connect nodes for offset
     cmds.connectAttr('{}.wm[0]'.format(sel[0]), '{}_mtxOffset.i[1]'.format(sel[1]), f = True)
     cmds.connectAttr('{}.wim[0]'.format(sel[1]), '{}_mtxOffset.i[2]'.format(sel[1]), f = True)
@@ -30,10 +29,32 @@ def nodalParentConstraint(*args):
     cmds.connectAttr('{}.wim[0]'.format(sel[0]), '{}_tempForOffset.i[1]'.format(sel[1]), f = True)
     offset = cmds.getAttr('{}_tempForOffset.o'.format(sel[1]))
     cmds.setAttr('{}_mtxOffset.i[0]'.format(sel[1]), offset, type = 'matrix')
-    cmds.delete('{}_tempForOffset'.format(sel[1]))
+    cmds.delete('{}_tempForOffset'.format(sel[1]))    
     
-    # Clean up connections
-    cmds.connectAttr('{}.pim[0]'.format(sel[1]), '{}_mtxOffset.i[2]'.format(sel[1]), f = True)
-    cmds.connectAttr('{}_mtx2srt.ot'.format(sel[1]), '{}.t'.format(sel[1]), f = True)
-    cmds.connectAttr('{}_mtx2srt.or'.format(sel[1]), '{}.r'.format(sel[1]), f = True)
-    cmds.connectAttr('{}_mtx2srt.os'.format(sel[1]), '{}.s'.format(sel[1]), f = True)
+    # If / else statement to check if the driven node is a joint
+    if cmds.objectType(sel[1], i = 'joint'):
+        # Create quaternion nodes
+        cmds.shadingNode('quatProd', asUtility = True, n = '{}_jointOrient_quatProd'.format(sel[1]))
+        cmds.shadingNode('quatInvert', asUtility = True, n = '{}_jointOrient_quatInvert'.format(sel[1]))
+        cmds.shadingNode('eulerToQuat', asUtility = True, n = '{}_jointOrient_euler2Quat'.format(sel[1]))
+        cmds.shadingNode('quatToEuler', asUtility = True, n = '{}_jointOrient_quat2Euler'.format(sel[1]))
+        
+        # Connect quaternion nodes
+        cmds.connectAttr('{}_mtx2srt.oq'.format(sel[1]), '{}_jointOrient_quatProd.iq1'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_jointOrient_quatInvert.oq'.format(sel[1]), '{}_jointOrient_quatProd.iq2'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_jointOrient_euler2Quat.oq'.format(sel[1]), '{}_jointOrient_quatInvert.iq'.format(sel[1]), f = True)
+        cmds.connectAttr('{}.jo'.format(sel[1]), '{}_jointOrient_euler2Quat.irt'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_jointOrient_quatProd.oq'.format(sel[1]), '{}_jointOrient_quat2Euler.iq'.format(sel[1]), f = True)
+        
+        # Clean up connections
+        cmds.connectAttr('{}.pim[0]'.format(sel[1]), '{}_mtxOffset.i[2]'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_mtx2srt.ot'.format(sel[1]), '{}.t'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_jointOrient_quat2Euler.ort'.format(sel[1]), '{}.r'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_mtx2srt.os'.format(sel[1]), '{}.s'.format(sel[1]), f = True)
+        
+    else:
+        # Clean up connections
+        cmds.connectAttr('{}.pim[0]'.format(sel[1]), '{}_mtxOffset.i[2]'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_mtx2srt.ot'.format(sel[1]), '{}.t'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_mtx2srt.or'.format(sel[1]), '{}.r'.format(sel[1]), f = True)
+        cmds.connectAttr('{}_mtx2srt.os'.format(sel[1]), '{}.s'.format(sel[1]), f = True)
